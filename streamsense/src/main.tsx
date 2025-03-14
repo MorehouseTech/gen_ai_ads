9d6f5664080a434c262600d0f9a955d38bb759e6
AJEgwu <84195094+AJEgwu@users.noreply.github.com>
2025-03-14 04:40:34 -0500

    [Aren] Adding changed files to properly deploy.

--- streamsense/src/main.tsx
+++ streamsense/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import '@fontsource/roboto/300.css';
 import Netflix from './pages/Netflix.tsx'
 import AIinfo from './pages/AIinfo.tsx'
@@ -10,45 +10,20 @@ import AdPage from './pages/AdPage.tsx'
 import Hulu from './pages/Hulu.tsx'
 import Youtube from './pages/Youtube.tsx'
 import PrimeVideo from './pages/PrimeVideo.tsx'
-//router imports
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-    {
-      path: "/Netflix",
-      element: <Netflix />
-    },
-    {
-      path: "/Hulu",
-      element: <Hulu />
-    },
-    {
-      path: "/Hulu/:passed",
-      element: <Hulu />
-    },
-    {
-      path: "/AIinfo/:title/:category/:company/:product",
-      element: <AIinfo />
-    },
-    {
-      path: "/AdPage/:title/:category",
-      element: <AdPage />
-    },
-    {
-      path: "/Youtube",
-      element: <Youtube />
-    },
-    {
-      path: "/AmazonPrime",
-      element: <PrimeVideo />
-    }
-]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+    <BrowserRouter basename="/gen_ai_ads">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Netflix" element={<Netflix />} />
+        <Route path="/Hulu" element={<Hulu />} />
+        <Route path="/Hulu/:passed" element={<Hulu />} />
+        <Route path="/AIinfo/:title/:category/:company/:product" element={<AIinfo />} />
+        <Route path="/AdPage/:title/:category" element={<AdPage />} />
+        <Route path="/Youtube" element={<Youtube />} />
+        <Route path="/AmazonPrime" element={<PrimeVideo />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
